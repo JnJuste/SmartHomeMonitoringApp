cd6f65fa04b2453a97c8603fae38f4d8f24d9e8d
JnJuste <jeanjusteirakoze@gmail.com>
2024-04-10 19:52:47 +0200

    Home Page

--- lib/home_Page.dart
+++ lib/home_Page.dart
@@ -29,28 +29,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _navigateBottomBar,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.gps_fixed),
-            label: "GPS Tracker",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.light),
-            label: "Light Sensor",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_walk_outlined),
-            label: "Accelerometer",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.compass_calibration),
-            label: "Magnetometer",
-          ),
-        ],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomBar,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.gps_fixed),
+              label: "GPS Tracker",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.light),
+              label: "Light Sensor",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_walk_outlined),
+              label: "Accelerometer",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.compass_calibration),
+              label: "Magnetometer",
+            ),
+          ],
+        ),
       ),
     );
   }
